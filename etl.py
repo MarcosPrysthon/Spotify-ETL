a696a06292441001eba879dd1adb2d42d5465a28
Marcos Vinicius Prysthon Nascimento <marcosvn@motorola.com>
2022-10-23 00:32:17 -0300

    Completing ETL process loading data into database on-premise

--- etl.py
+++ etl.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 import sqlite3
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import MetaData, Table, Column, Integer, String
 
 from datetime import datetime
 import datetime
@@ -29,7 +30,7 @@ def validate_data(df: pd.DataFrame):
         return False
     
     # primary key check
-    if not pd.Series(df['Played at']).is_unique:
+    if not pd.Series(df['Played_at']).is_unique:
         raise Exception("Primary Key Check violated - Duplicate values")
 
     # check for missing data
@@ -45,6 +46,8 @@ load_dotenv()
 DATABASE_LOCATION = os.getenv('DATABASE_LOCATION')
 USER_ID = os.getenv('USER_ID')
 TOKEN = os.getenv('TOKEN')
+
+# Extract
 
 headers = {
     'Accept': 'application/json',
@@ -79,14 +82,53 @@ for song in data['items']:
 songs_dict = {
     'Name': song_name,
     'Artist': artist_name,
-    'Played at': played_at,
+    'Played_at': played_at,
     'Timestamp': song_timestamp
 }
 
 song_df = pd.DataFrame(songs_dict)
-print(song_df)
 
+# Transform
 # Validate df
 if validate_data(song_df):
     print("Data is valid. Proceed to Load stage")
     print(song_df)
+
+# Load
+engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+conn = sqlite3.connect('played_tracks.sqlite')
+cursor = conn.cursor()
+
+# creating table using SQLAchemy
+meta = MetaData()
+played_tracks = Table(
+    'played_tracks', meta,
+    Column('name', String),
+    Column('artist', String),
+    Column('played_at', String, primary_key = True),
+    Column('timestamp', String)
+)
+
+meta.create_all(engine)
+
+# creating table using pure SQL query
+# sql_query = """
+# CREATE TABLE IF NOT EXISTS played_tracks(
+#     Name VARCHAR(200),
+#     Artist VARCHAR(200),
+#     Played_at VARCHAR(200),
+#     Timestamp VARCHAR(200),
+#     CONSTRAINT primary_key_constraint PRIMARY KEY (Played_at)
+# )
+# """
+
+#cursor.execute(sql_query)
+
+try:
+    song_df.to_sql('played_tracks', engine, index=False, if_exists='append')
+    print('Added data to database')
+except:
+    print('Data already exists in database')
+
+conn.close()
+print('Closed connection with database')
